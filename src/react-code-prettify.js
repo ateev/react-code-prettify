0d4f04f14907d23f6a1aaf9232ee169a40845874
Kyle Goss <foreseemed@Kyles-MacBook-Pro.local>
2017-12-06 07:52:22 -0800

    Added support for language="html"

--- src/react-code-prettify.js
+++ src/react-code-prettify.js
@@ -3,9 +3,24 @@ import prettify from 'code-prettify';
 import {TomorrowNight} from './styles';
 
 class Code extends React.Component {
+    htmlEscape(s) {
+        return s
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+    }
+
     render() {
-        const codeData = prettify.prettyPrintOne(this.props.codeString, this.props.language);
+        let codeData = prettify.prettyPrintOne(this.props.codeString);
+
+        // If language is set to 'html' replace all html entities with character codes
+        if(this.props.language == 'html'){
+            const codeDataArray = this.props.codeString.split('');
+            const codeEscaped = codeDataArray.map(letter => this.htmlEscape(letter)).join('');
+            codeData = prettify.prettyPrintOne(codeEscaped);
+        }
         const codeClassName = this.props.language !== undefined ? `language-${this.props.language}` : undefined;
+
         return (
             <TomorrowNight>
                 <pre className="prettyprint">
